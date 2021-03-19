5a8e3451d18d1d3b357a8fbc29d881135a7fcb82
Alejandra Salmerón <70578209+AlejandraSC@users.noreply.github.com>
2021-03-19 10:30:50 +0100

    Create all the components and add header details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Imported components
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { PromisesComponent } from './components/promises/promises.component';
+import { TopProductsComponent } from './components/top-products/top-products.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CategoriesComponent,
+    BrandsComponent,
+    PromisesComponent,
+    TopProductsComponent,
+    BlogComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
